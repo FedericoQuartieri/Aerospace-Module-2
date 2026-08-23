11bfe15cf81869391a19c6abcc9fe82a37a02db9
lechebs <lucaballone02@gmail.com>
2026-08-23 02:42:39 +0200

    reimplemented correct(), reproduced N2 heating test

--- tutorials/shockThermo/shockTube/plot_results_refactor.py
+++ tutorials/shockThermo/shockTube/plot_results_refactor.py
@@ -21,30 +21,23 @@ def save_semilogx_single(y: np.ndarray, label: str, ylabel: str, marker: str, ou
 def save_temperature_plot(out_png: str):
     plt.figure(figsize=(7.2, 4.8))
     T = data["T"][mask] / 1000.0
-    plt.semilogx(time, T, marker="+", linestyle="-", linewidth=1.0, markersize=4, label="T_tr")
+    plt.semilogx(time, T, markersize=4, label="T_tr")
 
     if "Tve" in data.dtype.names:
         Tve = data["Tve"][mask] / 1000.0
         plt.semilogx(
             time,
             Tve,
-            marker="o",
-            markerfacecolor="none",
-            linestyle="-",
-            linewidth=1.0,
             markersize=4,
-            label="T_v"
+            label="T_ve"
         )
 
     plt.xlabel("Time, t [s]")
     plt.ylabel(r"Temperature [$10^3$ K]")
-    plt.xlim(1e-9, 1e-4)
+    plt.xlim(1e-9, 2.5e-5)
     plt.legend(frameon=True, loc="best")
     plt.tight_layout()
     plt.savefig(out_png, dpi=300)
     plt.close()
 
-# Same “paper-ish” feel: log-x + markers only
-save_semilogx_single(data["p"][mask], "p", "Pressure, p [Pa]", marker="v", out_png="p_vs_time.png")
-save_temperature_plot(out_png="T_Tve_vs_time.png")
-save_semilogx_single(data["rho"][mask], "rho", r"Density, $\rho$ [kg/m$^3$]", marker="o", out_png="rho_vs_time.png")
+save_temperature_plot(out_png="Ttr_Tve.png")
